ca426bfcb3992b9e4c2d59ebeb0cedcd82788fa0
Aditya Pandey <aditya.pandey@printi.com.br>
2018-06-12 04:26:17 -0700

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name 'John Doe' },
+    { id: 22, name 'Jon Span' },
+    { id: 33, name 'John Brad' },
+    { id: 44, name 'Jon Mike' },
+    { id: 55, name 'Tom Roon' }
+];
